8e983d30a67ed1834f0c33c6acd11e5e71159c55
AvocatViorel <avocatialex2012@gmail.com>
2026-03-03 15:24:04 +0200

    Delete functions directory

--- functions/en/diaspora.js
+++
@@ -1,5 +0,0 @@
-const { handle } = require('../_diaspora_list');
-export async function onRequest({ env }){
-  const html = await handle({ env, lang: 'en', path: '/diaspora' });
-  return new Response(html, { headers: { 'content-type':'text/html; charset=utf-8' } });
-}
--- functions/index.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest({ request }){
-  const url = new URL(request.url);
-  return Response.redirect(url.origin + "/ro", 302);
-}
