3c40bd3112250a637bb96838e1ea95008e14a29b
AvocatViorel <avocatialex2012@gmail.com>
2026-03-03 17:08:12 +0200

    Delete functions directory

--- functions/ru.js
+++
@@ -1,2 +0,0 @@
-import { renderHome } from "./_render_home";
-export async function onRequest({ env }){ return new Response(await renderHome(env,"ru"), { headers: { "content-type":"text/html; charset=utf-8" } }); }
